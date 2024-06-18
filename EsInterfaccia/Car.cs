27a08a89fb0bc7d09c2dc6efcd39a7c67900758f
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2024-06-18 13:08:44 +0200

    Add project files.

---
+++ EsInterfaccia/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsInterfaccia
+{
+    internal class Car : IVehicle
+    {
+        public int Gasoline { get; set; }
+
+        public Car(int g)
+        {
+            Gasoline = g;
+        }
+
+        public void Drive()
+        {
+            if (Gasoline > 0)
+                Console.WriteLine("The car is Driving.");
+            else
+                Console.WriteLine("The car need a refuel!");
+        }
+
+        public bool Refuel(int n)
+        {
+            Gasoline += n;
+
+            if (Gasoline > 0)
+                return true;
+            else 
+                return false;
+        }
+
+    }
+}
---
+++ EsInterfaccia/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace EsInterfaccia
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        { 
+            var car = new Car(0);
+
+            car.Drive();
+
+            refuelCar(car);
+
+            
+        }
+
+        static void refuelCar(Car c)
+        {
+            bool isCheck = false;
+            var refuel = c.Refuel(c.Gasoline);
+
+            while (!isCheck)
+            {
+                Console.WriteLine("Quanto carburante vuoi rifornire?");
+                var input = Console.ReadLine();
+
+                Console.Clear();
+
+                var isInt = Int32.TryParse(input, out int n);
+
+                if ((!string.IsNullOrEmpty(input)) && (isInt))
+                {
+                    refuel = c.Refuel(n);
+                    isCheck = true;
+                }
+            }
+
+            if (refuel)
+                c.Drive();
+        }
+    }
+}
